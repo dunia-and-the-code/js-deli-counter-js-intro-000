e3f54693c0e7329560ac196a65db5069367129d7
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-12 00:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,23 +29,6 @@ function currentLine(line) {
 }
 
 
-/////////////////////////////////
-function currentLine(line) {
-  if (line.length === 0) {
-    return "The line is currently empty.";
-  } else {
-     var string = "The line is currently: ";
-     for (var i = line.length - 1; i >= 0; i--) {
-       if (i != 0) {
-         string = string + (i +1) + ". " + line[i] + ", ";
-       } else {
-          string = string + (i + 1) + ". " + line[i];
-       }
-    }
-    return string;
-  }
-}
-
 
 
 
